dd40c96db35ad12f3a5a6c0c48abed34765ea6df
Josh Allan <allan.josh07@gmail.com>
2021-01-25 23:34:39 -0500

    Connected the server to MongoDB

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/rvagrubs',
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    }
+  );
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const path = require('path');
+const colors = require('colors');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+
+// Connect Database
+connectDB();
+
+// Load env vars
+dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 8080;
 
 // Body parser
 app.use(express.json({ extended: false }));
+
+// cookie parse
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 8080;
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Set static folder
+// app.use(express.static(path.join(__dirname, 'client/public')));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`The server is listening at http://localhost:${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`
+      .yellow.bold
+  )
+);
